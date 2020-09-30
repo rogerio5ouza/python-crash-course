7af8901808806d9bbf81a3ab75c26f4a29004af6
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-09-30 10:04:22 -0300

    Create personal_message.py

---
+++ src/Capitulo_2/Try_It_Yourself/personal_message.py
@@ -0,0 +1,5 @@
+# Mensagem Pessoal: use uma variável para representar o nome de uma pessoa e imprima uma mensagem para essa pessoa.
+# Sua mensagem deve ser simples, como "Olá Eric, gostaria de aprender um pouco de Python hoje?"
+
+nome = "eric"
+print(f"Olá {nome.title()}, gostaria de aprender um pouco de Python hoje?")
