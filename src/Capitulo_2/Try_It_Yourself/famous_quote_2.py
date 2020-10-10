42b5894fc733ad125c1640e110e3eb242322f47f
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-10-10 13:26:54 -0300

    Create famous_quote_2.py

---
+++ src/Capitulo_2/Try_It_Yourself/famous_quote_2.py
@@ -0,0 +1,11 @@
+"""
+Famous Quote
+
+Repita o exercício 'famous_quote', mas dessa vez, represente o nome da pessoa famosa usando uma variável chamada 'famous_person'.
+Em seguida, componha sua mensagem e represente-a com uma nova variável chamada mensagem. Imprima sua mensagem.
+"""
+
+famous_person = "-Platão"
+message = '"O começo é a parte mais difícil do trbalho."'
+
+print(f"{message}\n{famous_person}")
