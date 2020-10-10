146b6c98b31cfc44792dae781078bcd5e28113e3
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-10-10 13:06:56 -0300

    Create famous_quote.py

---
+++ src/Capitulo_2/Try_It_Yourself/famous_quote.py
@@ -0,0 +1,9 @@
+"""
+Famous Quote
+
+Encontre uma frase de uma pessoa famosa que voçê admira. 
+Imprima a citação e o nomede seu autor.
+"""
+
+famous_quote = '"Os homens dividem-se em dois grupos:\n os que seguem em frente e fazem alguma coisa, e os que vão atrás a criticar."\n - Sêneca'
+print(famous_quote)
