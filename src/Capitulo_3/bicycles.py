1750cc6d80c3971338b217dc873404e7ef26e3af
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-10-12 21:18:54 -0300

    Create bicycles.py

---
+++ src/Capitulo_3/bicycles.py
@@ -0,0 +1,11 @@
+# Os colchetes ([]) indicam uma lista e os elementos individuais da lista são separados por vírgulas.
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles)
+
+# Acessando elementos em uma lista
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles[0])
